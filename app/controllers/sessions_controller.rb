958add855231c9fceeacd967ce89bcb5eaf20a3d
Andrew Waller <andrewhwaller@gmail.com>
2019-09-18 23:35:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-
+  skip_before_action :authenticate_user
   def create
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
       req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
